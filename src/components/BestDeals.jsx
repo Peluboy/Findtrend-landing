9b934998c7dd8a35e04c9f90b299fad4242ef50c
peluboy <imulep2104@gmail.com>
2023-07-05 20:50:22 +0100

    fixed the layout

--- src/components/BestDeals.jsx
+++ src/components/BestDeals.jsx
@@ -1,104 +1,131 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { motion } from 'framer-motion'
-import '../styles/bestDeals.css'
-import { Col, Container, Row } from 'reactstrap'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import "../styles/bestDeals.css";
+import { Col, Container, Row } from "reactstrap";
 
-import Deals from '../../src/assets/images/arrow-4.svg'
-
+import Deals from "../../src/assets/images/arrow-4.svg";
 
 const BestDeals = () => {
-
   const [darkMode, setDarkMode] = useState(false);
 
   return (
-    <section className='deals'>
-        <Container>
-            <Row>
-                <Col>
-                    <div className="best_deals">
-                        <h3>Get your best Deals</h3>
-                        <div className="monthly-yearly">
-                            <h5>Monthly</h5>
-                            <div className="theme-toggle">
-                                <label class="switch">
-                                    <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
-                                    <span class="slider round"></span>
-                                </label>
-                            </div>
-                            <h5>Yearly</h5>
-                            <img src={Deals} alt='' style={{ right: -110, top:-15 }}/>
-                        </div>
-                        <div className="subscriptions">
-                            <motion.div className="personal" whileHover={{scale: 1.03}}>
-                                <h4>Personal</h4>
-                                <p>Special first package for all</p>
-                                <hr />
-                                <div className="amt_monthly">
-                                    <h4>$8</h4>
-                                    <p>/ Month</p>
-                                </div>
-                                <div className="bullet_points">
-                                    <ul>
-                                        <li>Up to 5 page each group</li>
-                                        <li>Up to 10 group pages</li>
-                                        <li>5 Days group page saved</li>
-                                    </ul>
-                                </div>
-                                <Link to='/register'>
-                  <motion.button whileTap={{scale: 1.1}} className="free_trial">Start Free Trial</motion.button>
-                </Link>
-                            </motion.div>
-                            <motion.div className="regular" whileHover={{scale: 1.03}}>
-                                <h4>Regular</h4>
-                                <p>Recommended for personal pro</p>
-                                <hr />
-                                <div className="amt_monthly">
-                                    <h4>$20</h4>
-                                    <p>/ Month</p>
-                                </div>
-                                <div className="bullet_points">
-                                    <ul>
-                                        <li>Up to 15 pages each group</li>
-                                        <li>Download up to 20 pages</li>
-                                        <li>Up to 10 group pages</li>
-                                        <li>15 Days group pages saved</li>
-                                    </ul>
-                                </div>
-                                <Link to='/register'>
-                                    <motion.button whileTap={{scale: 1.1}} className="free_trial">Start Free Trial</motion.button>
-                                </Link>
-                            </motion.div>
-                            <motion.div className="premium" whileHover={{scale: 1.03}}>
-                                <h4>Premium</h4>
-                                <p>Packet of Startup & Company</p>
-                                <hr />
-                                <div className="amt_monthly">
-                                    <h4>$48</h4>
-                                    <p>/ Month</p>
-                                </div>
-                                <div className="bullet_points">
-                                    <ul>
-                                        <li>Unlimited group pages</li>
-                                        <li>Unlimited download pages</li>
-                                        <li>Unlimited pages in each group</li>
-                                        <li>Customize sorting group pages</li>
-                                        <li>Customize group page name</li>
-                                        <li>30 Days group pages saved</li>
-                                    </ul>
-                                </div>
-                                <Link to='/register'>
-                                    <motion.button whileTap={{scale: 1.1}} className="free_trial">Start Free Trial</motion.button>
-                                </Link>
-                            </motion.div>
-                        </div>
+    <section className="deals">
+      <Container>
+        <Row>
+          <Col>
+            <div className="best_deals">
+              <h3>Get your best Deals</h3>
+              <div className="monthly-yearly">
+                <h5>Monthly</h5>
+                <div className="theme-toggle">
+                  <label class="switch">
+                    <input
+                      type="checkbox"
+                      onChange={() => setDarkMode(!darkMode)}
+                    />
+                    <span class="slider round"></span>
+                  </label>
+                </div>
+                <h5>Yearly</h5>
+                <img src={Deals} alt="" style={{ right: -110, top: -15 }} />
+              </div>
+              <div className="subscriptions">
+                <motion.div className="personal" whileHover={{ scale: 1.03 }}>
+                  <div>
+                    <h4>Personal</h4>
+                    <p>Special first package for all</p>
+                    <hr />
+                    <div className="amt_monthly">
+                      <h4>$8</h4>
+                      <p>/ Month</p>
                     </div>
-                </Col>
-            </Row>
-        </Container>
-      
+                    <div className="bullet_points">
+                      <ul>
+                        <li>Up to 5 page each group</li>
+                        <li>Up to 10 group pages</li>
+                        <li>5 Days group page saved</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div>
+                    <Link to="/register">
+                      <motion.button
+                        whileTap={{ scale: 1.1 }}
+                        className="free_trial"
+                      >
+                        Start Free Trial
+                      </motion.button>
+                    </Link>
+                  </div>
+                </motion.div>
+                <motion.div className="regular" whileHover={{ scale: 1.03 }}>
+                  <div>
+                    <h4>Regular</h4>
+                    <p>Recommended for personal pro</p>
+                    <hr />
+                    <div className="amt_monthly">
+                      <h4>$20</h4>
+                      <p>/ Month</p>
+                    </div>
+                    <div className="bullet_points">
+                      <ul>
+                        <li>Up to 15 pages each group</li>
+                        <li>Download up to 20 pages</li>
+                        <li>Up to 10 group pages</li>
+                        <li>15 Days group pages saved</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div>
+                    <Link to="/register">
+                      <motion.button
+                        whileTap={{ scale: 1.1 }}
+                        className="free_trial"
+                      >
+                        Start Free Trial
+                      </motion.button>
+                    </Link>
+                  </div>
+                </motion.div>
+                <motion.div className="premium" whileHover={{ scale: 1.03 }}>
+                  <div>
+                    <h4>Premium</h4>
+                    <p>Packet of Startup & Company</p>
+                    <hr />
+                    <div className="amt_monthly">
+                      <h4>$48</h4>
+                      <p>/ Month</p>
+                    </div>
+                    <div className="bullet_points">
+                      <ul>
+                        <li>Unlimited group pages</li>
+                        <li>Unlimited download pages</li>
+                        <li>Unlimited pages in each group</li>
+                        <li>Customize sorting group pages</li>
+                        <li>Customize group page name</li>
+                        <li>30 Days group pages saved</li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div>
+                    <Link to="/register">
+                      <motion.button
+                        whileTap={{ scale: 1.1 }}
+                        className="free_trial"
+                      >
+                        Start Free Trial
+                      </motion.button>
+                    </Link>
+                  </div>
+                </motion.div>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </section>
-  )
-}
+  );
+};
 
-export default BestDeals
+export default BestDeals;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,54 +1,76 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { motion } from 'framer-motion'
-import Helmet from '../components/Helmet/Helmet'
-import { Container, Row, Col } from 'reactstrap'
-import '../styles/home.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import Helmet from "../components/Helmet/Helmet";
+import { Container, Row, Col } from "reactstrap";
+import "../styles/home.css";
 
-import Research from '../../src/assets/images/arrow-1.svg'
-import Twitter from '../../src/assets/images/twitter.svg'
-import Pinterest from '../../src/assets/images/pinterest.svg'
-import Facebook from '../../src/assets/images/facebook.svg'
-import Dribble from '../../src/assets/images/dribble.svg'
+import Research from "../../src/assets/images/arrow-1.svg";
+import Twitter from "../../src/assets/images/twitter.svg";
+import Pinterest from "../../src/assets/images/pinterest.svg";
+import Facebook from "../../src/assets/images/facebook.svg";
+import Dribble from "../../src/assets/images/dribble.svg";
 
-import Solutions from '../../src/assets/images/arrow-2.svg'
-import Need from '../../src/assets/images/arrow-3.svg'
-import Elon from '../../src/assets/images/elon.svg'
-import Startup from '../components/Startup'
-import Connect from '../components/Connect'
-import BestDeals from '../components/BestDeals'
-
-
-
+import Solutions from "../../src/assets/images/arrow-2.svg";
+import Need from "../../src/assets/images/arrow-3.svg";
+import Elon from "../../src/assets/images/elon.svg";
+import Startup from "../components/Startup";
+import Connect from "../components/Connect";
+import BestDeals from "../components/BestDeals";
 
 const Home = () => {
   return (
-    <Helmet title={'Home'}>
-
+    <Helmet title={"Home"}>
       <section className="hero_section">
         <Container>
           <Row>
             <Col>
               <div className="hero_content">
-                <h2 className="hero_heading">Minimize the tabs.<br/>Find the trend!</h2>
+                <h2 className="hero_heading">
+                  Minimize the tabs.
+                  <br />
+                  Find the trend!
+                </h2>
                 <p className="hero_paragraph">
-                  Don't let your computer memory consume all of those browser tabs.
-                  Findtrend let you gather all of your favourite websites into one place.</p>
+                  Don't let your computer memory consume all of those browser
+                  tabs. Findtrend let you gather all of your favourite websites
+                  into one place.
+                </p>
                 <div className="hero_button">
-                <Link to='/register'>
-                  <motion.button whileTap={{scale: 1.1}} className="get_started">Get Started 🔥</motion.button>
-                </Link>
-                  <img src={Research} alt='' />
+                  <Link to="/register">
+                    <motion.button
+                      whileTap={{ scale: 1.1 }}
+                      className="get_started"
+                    >
+                      Get Started 🔥
+                    </motion.button>
+                  </Link>
+                  <img src={Research} alt="" />
                 </div>
 
                 <div className="hero_img">
                   <div className="hero_images">
-                    <motion.img src={Twitter} alt='twitter' whileHover={{scale:1.1}}/>
-                    <motion.img src={Pinterest} alt='pinterest' whileHover={{scale:1.1}}/>
-                    <motion.img src={Facebook} alt='facebook' whileHover={{scale:1.1}}/>
-                    <motion.img src={Dribble} alt='dribble' whileHover={{scale:1.1}}/>
+                    <motion.img
+                      src={Twitter}
+                      alt="twitter"
+                      whileHover={{ scale: 1.1 }}
+                    />
+                    <motion.img
+                      src={Pinterest}
+                      alt="pinterest"
+                      whileHover={{ scale: 1.1 }}
+                    />
+                    <motion.img
+                      src={Facebook}
+                      alt="facebook"
+                      whileHover={{ scale: 1.1 }}
+                    />
+                    <motion.img
+                      src={Dribble}
+                      alt="dribble"
+                      whileHover={{ scale: 1.1 }}
+                    />
                   </div>
-
                 </div>
               </div>
             </Col>
@@ -59,19 +81,20 @@ const Home = () => {
       <section className="newTab_section">
         <Container>
           <Row>
-            <Col lg='12' md='6'>
+            <Col lg="12" md="6">
               <div className="newtab">
                 <h2>Open new tabs is sh*t</h2>
                 <div className="elon">
-                  <img src={Need} alt=''/>
+                  <img src={Solutions} alt="" />
                   <span>
-                    <img src={Elon} alt=''/>
+                    <img src={Elon} alt="" />
                   </span>
-                  <img src={Solutions} alt=''/>
+                  <img src={Need} alt="" />
                 </div>
                 <p>
-                  A solution for your browser tabs and don't make your device get slower
-                  over time. Get ease and faster to discover a trend with just one tab.
+                  A solution for your browser tabs and don't make your device
+                  get slower over time. Get ease and faster to discover a trend
+                  with just one tab.
                 </p>
               </div>
             </Col>
@@ -82,39 +105,44 @@ const Home = () => {
       <section className="findTrend_section">
         <Container>
           <Row>
-            <Col lg='12' md='6'>
-              <h3 className='findTrend'>
-                Findtrend helps you to increase your productivity and reduce your computer's
-                memory load,<span className='typing'> an application that can fulfill your daily browsing needs.</span> 
+            <Col lg="12" md="6">
+              <h3 className="findTrend">
+                Findtrend helps you to increase your productivity and reduce
+                your computer's memory load,
+                <span className="typing">
+                  {" "}
+                  an application that can fulfill your daily browsing needs.
+                </span>
               </h3>
             </Col>
           </Row>
         </Container>
       </section>
 
-      <Startup/>
+      <Startup />
 
       <Connect />
 
-      <BestDeals/>
+      <BestDeals />
 
       <section className="join_email">
         <Container>
           <Row>
-            <Col lg='12' md='6'>
-              <h3 className='join'>
-                Join us on email for<span> more trending topics</span> 
+            <Col lg="12" md="6">
+              <h3 className="join">
+                Join us on email for<span> more trending topics</span>
               </h3>
-              <Link to='/register'>
-                  <motion.button whileTap={{scale: 1.1}} className="join_now">Join Now</motion.button>
+              <Link to="/register">
+                <motion.button whileTap={{ scale: 1.1 }} className="join_now">
+                  Join Now
+                </motion.button>
               </Link>
             </Col>
           </Row>
         </Container>
       </section>
-
     </Helmet>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
